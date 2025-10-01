bffbff273b21e6a7f491f26327250d61e050440c
Narem, Bhavani Shankar <narrashankar998@gmail.com>
2025-10-02 00:06:47 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { RouterOutlet, Router } from "@angular/router";
 
 
 @Component({
@@ -8,5 +8,15 @@ import { RouterOutlet } from "@angular/router";
   imports: [RouterOutlet],
   template: `<router-outlet></router-outlet>`
 })
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
 
-export class AppComponent {}
+  ngOnInit() {
+    // Check for a redirect path in sessionStorage (set by 404.html)
+    const redirect = sessionStorage.getItem('redirect');
+    if (redirect && redirect !== window.location.pathname) {
+      sessionStorage.removeItem('redirect');
+      this.router.navigateByUrl(redirect);
+    }
+  }
+}
